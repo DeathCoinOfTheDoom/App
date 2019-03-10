1eeb9ee49d4d417d05bc586a421cc963b6371fb9
Gérome <gerome.lacaux@hetic.net>
2019-03-10 18:15:42 +0100

    add integration mission details (#29)
    Integration of mission details screen

---
+++ Bob/Views/FolderDetails/FolderDetailsVC.swift
@@ -0,0 +1,17 @@
+//
+//  FolderDetailsVC.swift
+//  Bob
+//
+//  Created by Gérome Lacaux on 20/02/2019.
+//  Copyright © 2019 Bob. All rights reserved.
+//
+
+import UIKit
+
+class FolderDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

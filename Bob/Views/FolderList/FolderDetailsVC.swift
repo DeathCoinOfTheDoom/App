d646a752c10900134f7ba622e4b54b90aa2503e0
Gérome Lacaux <gerome.lacaux@hetic.net>
2019-04-02 16:02:59 +0200

    test

--- Bob/Views/FolderList/FolderDetailsVC.swift
+++ Bob/Views/FolderList/FolderDetailsVC.swift
@@ -1,11 +1,3 @@
-//
-//  FolderDetails.swift
-//  Bob
-//
-//  Created by Victor Lucas on 28/01/2019.
-//  Copyright © 2019 Bob. All rights reserved.
-//
-
 import UIKit
 
 class FolderDetailsVC: ImagePickerViewController {

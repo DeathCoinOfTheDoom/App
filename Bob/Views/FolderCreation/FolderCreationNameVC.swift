64d0f1c0a546f36a1befbaf1bb7a1c1c1988f2a4
mona <mona.calibani@hetic.net>
2019-04-03 10:18:40 +0200

    new folder storyboard and view creation WIP

---
+++ Bob/Views/FolderCreation/FolderCreationNameVC.swift
@@ -0,0 +1,22 @@
+//
+//  FolderCreationViewController.swift
+//  Bob
+//
+//  Created by Isabelle Melchiori on 02/04/2019.
+//  Copyright © 2019 Bob. All rights reserved.
+//
+import UIKit
+
+class FolderCreationNameVC: UIViewController {
+    
+}
+
+
+/*
+ // MARK: - Navigation
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */

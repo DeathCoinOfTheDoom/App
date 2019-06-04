2b7ad1ad190ea1edec6c770974784f561205028a
Gérome <gerome.lacaux@hetic.net>
2019-06-04 19:07:37 +0200

    views: appInit, CategoryDetails, CategoryList (#85)

--- Bob/Views/CategoryDetails/CategoryDetailsCard.swift
+++ Bob/Views/CategoryDetails/CategoryDetailsCard.swift
@@ -1,11 +1,3 @@
-//
-//  FolderDetailsCard.swift
-//  Bob
-//
-//  Created by Gérome Lacaux on 20/02/2019.
-//  Copyright © 2019 Bob. All rights reserved.
-//
-
 import UIKit
 
 class CategoryDetailsCard: UIView {
